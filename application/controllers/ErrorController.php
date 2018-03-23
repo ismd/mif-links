bc6a321d7c60cf08e7007c396014be5a8f85779f
Vladimir Kosteley <zzismd@gmail.com>
2018-03-23 16:07:28 +0300

    refactoring error controller

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -11,21 +11,21 @@ class ErrorController extends PsController {
             'l.short_link' => $this->registry->router->getRoute(),
         ], 1);
 
-        if (!empty($link)) {
-            $link = $link[0];
-
-            StatMapper::getInstance()->add($link['id'], [
-                'referer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null,
-                'user_agent' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null,
-                'ip' => $_SERVER['REMOTE_ADDR'],
-            ]);
-
-            http_response_code(301);
-            header('Location: ' . $link['link']);
+        if (empty($link)) {
+            header('HTTP/1.0 404 Not Found', true, 404);
             die;
         }
 
-        header('HTTP/1.0 404 Not Found', true, 404);
+        $link = $link[0];
+
+        StatMapper::getInstance()->add($link['id'], [
+            'referer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null,
+            'user_agent' => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null,
+            'ip' => $_SERVER['REMOTE_ADDR'],
+        ]);
+
+        http_response_code(301);
+        header('Location: ' . $link['link']);
         die;
     }
 }
